160f20dc68dcecc36390b79a8cdc3e86600ec6b1
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-06-27 07:19:12 +0530

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import Test from "./components/Test";
-import Welcome from "./components/Welcome.jsx";
+import Welcome from "./components/Welcome";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Settings from "./components/Settings";
+import Profiles from "./components/Profiles";
+import DashBoard from "./components/DashBoard";
 
 function App() {
   return (
     <div className="app-container">
+      <Navbar />
       <Welcome />
+      <Home />
+      <DashBoard />
+      <Profiles />
+      <Settings />
       <Test />
     </div>
   );
